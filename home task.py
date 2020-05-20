ffad3f482ef021bc52f53150c3e5da43d18d4616
Nooruz2203 <61961630+Nooruz2203@users.noreply.github.com>
2020-05-20 17:56:54 +0600

    Update home task.py

--- home task.py
+++ home task.py
@@ -17,4 +17,29 @@ def solution(A, B):
 
     return go_up_count + len(s1)
 
+#     def solution(A, B):
     
+#     downStack = [0] * len(A)
+#     downSize = 0
+    
+#     upAlive = 0
+    
+#     for i in range(-1, -1*(len(A)+1), -1):
+        
+#         if B[i] is 0: 
+#             downStack[downSize] = A[i]
+#             downSize += 1
+#         else: 
+            
+#             if downSize is 0: 
+#                 upAlive += 1
+#             else: 
+                
+#                 while A[i] > downStack[downSize-1]:
+#                     downSize -= 1
+#                     if downSize is 0:
+#                         break
+#                 if downSize is 0:
+#                     upAlive += 1
+    
+#     return downSize+upAlive
